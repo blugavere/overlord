c463d3edc15fd97bb31fb14fe12729b792051102
blugavere <b.lugavere@gmail.com>
2016-12-03 13:21:29 -0600

    logging

--- services/HydraliskService/app.js
+++ services/HydraliskService/app.js
@@ -1,14 +1,15 @@
 
 'use strict';
 
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
 const name = 'Hydralisk Service';
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  console.log(`${name} Route Hit!`);
+  res.send(`Hello World! - ${name}`);
+});
 
 app.listen(3001, function () {
   console.log(`${name} listening on port 3001!`);
